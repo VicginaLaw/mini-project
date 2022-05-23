ca2de7dcaa619852e79c18ccfa98cf6d4a29e800
VicginaLaw <104979683+VicginaLaw@users.noreply.github.com>
2022-05-23 15:04:08 +0100

    Update W6-cafe-app.py

--- W6-cafe-app.py
+++ W6-cafe-app.py
@@ -1,4 +1,5 @@
 import pymysql
+from sqlalchemy import true
 from tabulate import tabulate
 
 connection = pymysql.connect(
@@ -23,97 +24,185 @@ def view_couriers():
     sql = "SELECT * FROM Couriers"
     cursor.execute(sql)
     result = cursor.fetchall()
-    print ("\nHere is the Product List: ")
+    print ("\nHere is the Courier List: ")
     col_names = ["Courier_id", "Name", "Phone"]
     print(tabulate(result, headers=col_names))
     cursor.close()
 
 def view_orders():
-    cursor = connection.cursor()
-    sql = "SELECT * FROM Orders"
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    print ("\nHere is the Product List: ")
-    col_names = ["Order_id", "Customer_name", "Customer_address", "Phone_number", "Courier", "Status", "Items"]
-    print(tabulate(result, headers=col_names))
-    cursor.close()
+    while true:
+        print ("\n----- View Order List -----")
+        print ("[0] By Order ID")
+        print ("[1] By Status")
+        print ("[2] By Courier")
+        print ("[3] Customer List\n")
+        option = input("\nChoose an option: ")
+
+        if option == "0":
+            cursor = connection.cursor()
+            sql = "SELECT * FROM Orders"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print ("\nHere is the Order List: ")
+            col_names = ["Order_id", "Customer_name", "Customer_address", "Phone_number", "Courier", "Status", "Items"]
+            print(tabulate(result, headers=col_names))
+            cursor.close()
+            break
+
+        elif option == "1":
+            cursor = connection.cursor()
+            sql = "SELECT * FROM Orders ORDER BY Status"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print ("\nHere is the Order List: ")
+            col_names = ["Order_id", "Customer_name", "Customer_address", "Phone_number", "Courier", "Status", "Items"]
+            print(tabulate(result, headers=col_names))
+            cursor.close()
+            break
+
+        elif option == "2":
+            cursor = connection.cursor()
+            sql = "SELECT * FROM Orders ORDER BY Courier"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print ("\nHere is the Order List: ")
+            col_names = ["Order_id", "Customer_name", "Customer_address", "Phone_number", "Courier", "Status", "Items"]
+            print(tabulate(result, headers=col_names))
+            cursor.close()
+            break
+
+        elif option == "3":
+            cursor = connection.cursor()
+            sql = "SELECT order_id, customer_name, customer_address, phone_number FROM Orders"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print ("\nHere is the Order List: ")
+            col_names = ["Order_id", "Customer_name", "Customer_address", "Phone_number"]
+            print(tabulate(result, headers=col_names))
+            cursor.close()
+            break
+
+        else: 
+            invaild()
+            continue
 
 def view_order_status():
     cursor = connection.cursor()
     sql = "SELECT * FROM Order_status"
     cursor.execute(sql)
     result = cursor.fetchall()
-    print ("\nHere is the Product List: ")
+    print ("\nHere is the Order Status List: ")
     col_names = ["Order_status_id", "Order_status"]
     print(tabulate(result, headers=col_names))
     cursor.close()
 
 def add_new_product():
-    print ("\n----- Create New Product -----")
-    new_product = input("Add a new product: ").title().strip()
-    new_price = float (input(f"Set a price of {new_product}: ")) 
+    while true:
+        print ("\n----- Create New Product -----")
 
-    cursor = connection.cursor()
-    sql = (f"INSERT INTO Products SET Name = '{new_product}', Price = {new_price}")
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+        try:
+            new_product = input("Add a new product: ").title().strip()
+            new_price = float (input(f"Set a price of {new_product}: ")) 
+        
+        except: 
+            invaild()
+            continue
 
-    print (f"\n{new_product} is added.")
+        cursor = connection.cursor()
+        sql = (f"INSERT INTO Products SET Name = '{new_product}', Price = {new_price}")
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+
+        print (f"\n{new_product} is added.")
+        break
 
 def add_new_courier():
-    print ("\n----- Create New Courier -----")
-    new_courier = input("The name of new courier: ").title().strip()
-    new_phone = input(f"The phone number of {new_courier}: ").strip() 
+    while true:
+        print ("\n----- Create New Courier -----")
 
-    cursor = connection.cursor()
-    sql = (f"INSERT INTO Couriers SET Name = '{new_courier}', Phone = '{new_phone}'")
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+        new_courier = input("The name of new courier: ").title().strip()
+        new_phone = input(f"The phone number of {new_courier}: ").strip() 
 
-    print (f"\n{new_courier} is added.")
+        if new_courier == "" or new_phone == "": 
+            invaild()
+            print ("Don't leave blank for each question!")
+            continue
+
+        cursor = connection.cursor()
+        sql = (f"INSERT INTO Couriers SET Name = '{new_courier}', Phone = '{new_phone}'")
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+
+        print (f"\n{new_courier} is added.")
+        break
 
 def add_new_order():
-    print ("\n----- Create New Order -----")
-    Customer_name = input ("Customer Name:").title().strip()
-    Customer_address = input ("Customer Address:").title().strip()
-    Customer_phone = input ("Customer Phone:").strip()
+    while true:
+        print ("\n----- Create New Order -----")
 
-    view_products()
-    print("\nPlease Chooose the items! If more than one item, please use comma to separate them. e.g '1,3,4'")
-    Customer_product = input ("Type in the Product IDs:").strip()
+        name = input ("Customer Name: ").title().strip()
+        address = input ("Customer Address: ").title().strip()
+        phone = input ("Customer Phone: ").strip()
 
-    view_couriers()
-    Courier_index = int(input ("Courier Index: "))
+        if name == "" or address == "" or phone == "":
+            invaild()
+            print ("Don't leave blank for each question!")
+            continue
 
-    Status = 1
+        view_products()
+        print("\nPlease Chooose the items! If more than one item, please use comma to separate them. e.g '1,3,4'")
+        item = input ("Type in the Product IDs:").strip()
 
-    cursor = connection.cursor()
-    sql = (f"INSERT INTO Orders SET Customer_name = '{Customer_name}', Customer_address = '{Customer_address}', Phone_number = '{Customer_phone}', Courier = {Courier_index}, Status = 1, Items = '{Customer_product}'")
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+        if item == "" :
+            invaild()
+            print ("Don't leave blank for each question!")
+            continue
 
-    print ("\nThe Order is added.")
+        view_couriers()
+        try:
+            courier = int(input ("Courier ID [INDEX ONLY]: "))
+        except:
+            invaild()
+            print ("Don't leave blank for each question and INDEX ONLY!")
+            continue
+            
+
+        cursor = connection.cursor()
+        sql = (f"INSERT INTO Orders SET Customer_name = '{name}', Customer_address = '{address}', Phone_number = '{phone}', Courier = {courier}, Status = 1, Items = '{item}'")
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+
+        print ("\nThe Order is added.")
+        break
 
 def delete_product():
-    print ("\n----- Delete Product -----")
-    view_products()
-    del_item = int (input("\nType the ID of product need to delete: "))
+    while true:
+        try: 
+            print ("\n----- Delete Product -----")
+            view_products()
+            del_item = int (input("\nThe ID of product need to delete [INDEX ONLY]: "))
 
-    cursor = connection.cursor()
-    sql = (f"DELETE FROM Products WHERE Product_id = {del_item}")
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+        except:
+            invaild()
+            print("Please input the INDEX only.")
+            continue
 
-    print ("\nThe product is deleted.")
+        cursor = connection.cursor()
+        sql = (f"DELETE FROM Products WHERE Product_id = {del_item}")
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+
+        print ("\nThe product is deleted.")
+        break
 
 def delete_courier():
     print ("\n----- Delete Courier -----")
     view_couriers()    
-    del_item = int (input("\nType the ID of courier need to delete: "))
+    del_item = int (input("\nThe ID of courier need to delete: "))
 
     cursor = connection.cursor()
     sql = (f"DELETE FROM Couriers WHERE Courier_id = {del_item}")
@@ -124,90 +213,163 @@ def delete_courier():
     print ("\nThe Courier is deleted.")
 
 def delete_order():
-    print ("\n----- Delete Order -----")
-    view_orders()            
-    del_item = int (input ("\nType the ID of order need to delete: "))
+    while true:
+        try: 
+            print ("\n----- Delete Order -----")
+            view_orders()            
+            del_item = int (input("\nThe ID of order need to delete [INDEX ONLY]: "))
 
-    cursor = connection.cursor()
-    sql = (f"DELETE FROM Orders WHERE Order_id = {del_item}")
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+        except:
+            invaild()
+            print("Please input the INDEX only.")
+            continue
 
-    print("\nThe Order is deleted")
+        cursor = connection.cursor()
+        sql = (f"DELETE FROM Orders WHERE Order_id = {del_item}")
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+
+        print("\nThe Order is deleted")
+        break
 
 def update_order_status():
     print ("\n----- Update Existing Order Status -----")
     view_orders()            
-    order_update = int (input ("\nType the ID of order need to update: "))
+    order_update = int (input ("\nThe ID of order need to update: "))
 
     view_order_status()            
-    status_update = int (input ("\nType updated status ID: "))
+    status_update = int (input ("\nType in the updated status ID: "))
 
     cursor = connection.cursor()
     sql = (f"UPDATE Orders SET Status = {status_update} WHERE Order_id = {order_update}")
     cursor.execute(sql)
     connection.commit()
     cursor.close()
-    print("\nThe Order is Updated")
+    print("\nThe Order Status is Updated")
 
 def update_order():
-    print ("\n----- Update Existing Order -----")
+    while true:
+        try: 
+            print ("\n----- Update Existing Order Status -----")
+            view_orders()            
+            order_update = int (input ("\nThe ID of order need to update[INDEX ONLY]: "))
 
-    view_orders()
-    existing_order = int (input ("Please input the ID of product need to update: "))
+        except:
+            invaild()
+            print("Please input INDEX only!")
+            continue
 
-    Customer_name = input ("Customer Name:").title().strip()
-    Customer_address = input ("Customer Address:").title().strip()
-    Customer_phone = input ("Customer Phone:").strip()
+        try:
+            view_order_status()            
+            status_update = int (input ("\nType in the updated status ID: "))
+        except:
+            invaild()
+            print("Please input INDEX only!")
+            continue
 
-    view_products()
-    print("\nPlease Chooose the items! If more than one item, please use comma to separate them. e.g '1,3,4'")
-    Customer_product = input ("Type in the Product IDs:").strip()
-
-    view_couriers()
-    Courier_index = int(input ("Courier Index: "))
-
-    cursor = connection.cursor()
-    sql = f"UPDATE Orders SET Customer_name = '{Customer_name}', Customer_address = '{Customer_address}', Phone_number = '{Customer_phone}', Courier = {Courier_index}, Items = '{Customer_product}'  WHERE Order_id = {existing_order}"
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
-            
-    print ("The order is updated.")
+        cursor = connection.cursor()
+        sql = (f"UPDATE Orders SET Status = {status_update} WHERE Order_id = {order_update}")
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        
+        print("\nThe Order Status is Updated")
+        break
 
 def update_product():
-    print ("\n----- Update Existing Product -----")
-            
-    view_products()
-    existing_product = int (input ("Please input the ID of product need to update: "))
-    update_name = input ("Update Product Name: ").title().strip()
-    update_price = float (input ("Update Product Price: "))
+    while true:
+        try: 
+            print ("\n----- Update Existing Product -----")
 
-    cursor = connection.cursor()
-    sql = f"UPDATE Products SET Name = '{update_name}', Price = {update_price}  WHERE Product_id = {existing_product}"
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+            view_products()
+            existing_product = int (input ("\nThe ID of product need to update: "))
+            print ("\nPlease press ENTER to skip the details DONT'T NEED to update.")
+            update_name = input ("Update Product Name: ").title().strip()
+            update_price =  input ("Update Product Price: ")
 
-    print ("The Product is updated.")
+        except: 
+            invaild()
+            continue
+
+        if len(update_name) == 0 and len(update_price) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE Products SET Price = {float(update_price)} WHERE Product_id = {existing_product}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print ("\nThe Product is updated.")
+            break
+
+        elif len(update_price) == 0 and len(update_name) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE Products SET Name = '{update_name}' WHERE Product_id = {existing_product}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print ("\nThe Product is updated.")
+            break
+
+        elif len(update_name) > 0 and len(update_price) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE Products SET Name = '{update_name}', Price = {float(update_price)} WHERE Product_id = {existing_product}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print ("\nThe Product is updated.")
+            break
+
+        else: 
+            print ("Nothing updated.")
+            break
 
 def update_courier():
-    print ("\n----- Update Existing Courier -----")
-    view_couriers()
-    existing_couriers = int (input ("Please input the ID of courier need to update: "))
-    update_name = input ("Update Courier Name: ").title().strip()
-    update_phone = input ("Update Courier Phone: ")
+    while true:
+        try: 
+            print ("\n----- Update Existing Product -----")
+            view_couriers()
+            existing_couriers = int (input ("Please input the ID of courier need to update: "))
+            update_name = input ("Update Courier Name: ").title().strip()
+            update_phone = input ("Update Courier Phone: ").strip()
 
-    cursor = connection.cursor()
-    sql = f"UPDATE Couriers SET Name = '{update_name}', Phone = '{update_phone}'  WHERE Courier_id = {existing_couriers}"
-    cursor.execute(sql)
-    connection.commit()
-    cursor.close()
+        except: 
+            invaild()
+            continue
+
+        if len(update_name) == 0 and len(update_phone) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE Couriers SET Phone = '{update_phone}'  WHERE Courier_id = {existing_couriers}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print ("\nThe Courier is updated.")
+            break
+
+        elif len(update_phone) == 0 and len(update_name) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE Couriers SET Name = '{update_name}' WHERE Courier_id = {existing_couriers}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print ("\nThe Courier is updated.")
+            break
+
+        elif len(update_name) > 0 and len(update_phone) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE Couriers SET Name = '{update_name}', Phone = '{update_phone}' WHERE Courier_id = {existing_couriers}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print ("\nThe Courier is updated.")
+            break
+
+        else: 
+            print ("Nothing updated.")
+            break
 
 def Main_Menu():
     print ("\n----- Main Menu -----")
-    print ("[0] Save Change and Exit App")
+    print ("[0] Exit App")
     print ("[1] Products Menu")
     print ("[2] Couriers Menu")
     print ("[3] Order Menu\n")
@@ -231,17 +393,14 @@ def Order_Menu():
 
 def invaild():
     print ("\n----- Invaild Answer -----")
-    print ("Please enter a valid option! \n")
+    print ("Please enter a valid input!")
 
 while True:
     Main_Menu()
     
     try:
         mm = int( input("Choose an option: \n"))
-    except NameError: 
-        invaild()
-        continue
-    except ValueError:
+    except:
         invaild()
         continue
 
@@ -254,10 +413,7 @@ while True:
         try:
             pm = int( input("Choose an option: \n"))
             
-        except NameError: 
-            invaild() 
-            continue
-        except ValueError: 
+        except:
             invaild() 
             continue
 
@@ -282,10 +438,7 @@ while True:
         Print_Menu("Courier")
         try:
             cm = int( input("Choose an option: \n"))
-        except NameError: 
-            invaild()
-            continue
-        except ValueError:
+        except:
             invaild()
             continue
 
@@ -334,5 +487,7 @@ while True:
             delete_order()
 
         elif om > 5: invaild()
-
-    else: invaild()
+    
+    if mm > 3: 
+        invaild()
+        continue
